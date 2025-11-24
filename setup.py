9845b54167047ea328e56e360bd90fe219876d69
jchchiu <84771170+jchchiu@users.noreply.github.com>
2025-11-24 13:10:39 +1100

    feat: updated wbc to include Westpac Business One Plus Statement of Recent Transactions converter (#2)
    * feat: added Westpac Business One Statement of Recent Transactions converter
    
    * refactor: updated recent transactions; added warnings for no balance checks
    
    * docs: updated version and readme

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = 'bank_statement_converter',
-    version = '0.2.0',
+    version = '0.2.1',
     package_dir = {'':'src'},
     packages = find_packages('src'),
     install_requires = [
