9f4807340ec60a7c3abb8a1c6b1179fcb22ddc93
jchchiu <84771170+jchchiu@users.noreply.github.com>
2025-11-25 14:01:06 +1100

    fix: changed NAB transaction converter to get x coords dynamically (#4)
    * fix:
    
    - changed transaction converter to get x coords dynamically
    
    - updated to skip line if 'PLEASE NOTE FROM TODAY YOUR' in description
    
    * docs: updated version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = 'bank_statement_converter',
-    version = '0.3.0',
+    version = '0.3.1',
     package_dir = {'':'src'},
     packages = find_packages('src'),
     install_requires = [
