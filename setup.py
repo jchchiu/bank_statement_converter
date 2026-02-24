4b492baaa00ef0e9e00cf9990872ee7f6b780edf
jchchiu <julianchiu97@gmail.com>
2026-02-24 14:45:20 +1100

    docs: bumped version to 0.3.3

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = 'bank_statement_converter',
-    version = '0.3.2',
+    version = '0.3.3',
     package_dir = {'':'src'},
     packages = find_packages('src'),
     install_requires = [
